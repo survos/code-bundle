6db373fc65e655987b7627df3e097bca4df7f252
Tac Tacelosky <tacman@gmail.com>
2025-05-05 11:39:40 -0400

    initial version

---
+++ src/SurvosCodeBundle.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Survos\CodeBundle;
+
+use Survos\CodeBundle\Command\CodeCommand;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+
+class SurvosCodeBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->autowire(CodeCommand::class)
+            ->setArgument('$projectDir', new Reference('kernel.project_dir'))
+            ->addTag('console.command')
+            ->addTag('container.service_subscriber')
+        ;
+
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+            ->scalarNode('base_layout')->defaultValue('base.html.twig')->end()
+            ->end();
+    }
+
+}
